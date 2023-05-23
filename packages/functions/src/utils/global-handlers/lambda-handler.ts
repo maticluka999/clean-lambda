75f00851d7f99a24396b0f5c029f587d841bc106
Luka Matic <maticluka999@gmail.com>
2023-05-24 01:57:56 +0200

    added support for sst ApiHandler hooks

--- packages/functions/src/utils/global-handlers/lambda-handler.ts
+++ packages/functions/src/utils/global-handlers/lambda-handler.ts
@@ -5,6 +5,7 @@ import {
   Context,
 } from 'aws-lambda';
 import { handleError } from './error-handler';
+import { ApiHandler } from 'sst/node/api';
 
 /**
  * Custom lambda wrapper
@@ -17,11 +18,15 @@ export default function lambdaHandler(
 ): APIGatewayProxyHandlerV2 {
   return async function (event: APIGatewayProxyEventV2, context: Context) {
     try {
-      const { statusCode, headers, body } = await lambda(event, context);
+      const apiHandler = ApiHandler((event, context) => lambda(event, context));
+      const { statusCode, headers, body, isBase64Encoded, cookies } =
+        await apiHandler(event, context);
       return {
         statusCode: statusCode || 200,
         headers,
         body,
+        isBase64Encoded,
+        cookies,
       };
     } catch (e: any) {
       return handleError(e);
